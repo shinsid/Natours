7d99c5773ece484647dd66f01d6994ae18006195
Siddharth Shinde <siddharth.shinde@autodesk.com>
2023-02-03 17:12:54 -0500

    add middleware

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log('Hello from the middleware!');
+    next();
+});
+
 const port = 3000;
 
 // app.get('/', (req, res) => {
