81155d9cb0483b3b8c3846dbdcc35c0166cf3a0e
Siddharth Shinde <siddharth.shinde@autodesk.com>
2023-02-03 20:30:38 -0500

    add param middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,20 @@ const fs = require('fs');
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
+exports.checkID = (req, res, next, val) => {
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+    if(!tour)
+    {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid id'
+        });
+    }
+    next();
+};
+
 exports.getAllTours = (req, res) => {
-    console.log(req.requestTime);
     res.status(200).json({ 
         status: 'success',
         requestedAt: req.requestTime,
@@ -15,15 +27,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
-
-    if(!tour)
-    {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid id'
-        });
-    }
-
     res.status(200).json({ 
         status: 'success',
         data: {tour: tour}
@@ -31,10 +34,8 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-    
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({id: newId}, req.body);
-
     tours.push(newTour);
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
@@ -45,18 +46,8 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-    
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
-
-    if(!tour)
-    {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid id'
-        });
-    }
-
     res.status(200).json({
         status: 'success',
         data: {tour: 'Updated tour here...'} 
@@ -64,18 +55,8 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-    
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
-
-    if(!tour)
-    {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid id'
-        });
-    }
-
     res.status(204).json({
         status: 'success',
         data: null 
