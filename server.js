d839bb6c98d0c37af8fe35e1b733d91db8f68a82
Siddharth Shinde <siddharth.shinde@autodesk.com>
2023-03-04 01:49:15 -0500

    add tour schema and model

--- server.js
+++ server.js
@@ -16,7 +16,23 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
-console.log(process.env);
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 // START THE SERVER
 const port = process.env.PORT || 3000;
