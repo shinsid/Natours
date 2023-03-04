3bf8df0d438f94b193eeed5edf5605cc87a025ce
Siddharth Shinde <siddharth.shinde@autodesk.com>
2023-03-04 01:59:45 -0500

    chore: save testTour

--- server.js
+++ server.js
@@ -34,6 +34,21 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+  // rating: 4.7,
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 // START THE SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
