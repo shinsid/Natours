62013f6ce3d1248a93c5af5a5ead457e773f2e02
Siddharth Shinde <siddharth.shinde@autodesk.com>
2023-03-04 17:31:52 -0500

    chore: implement tour querying

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,26 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // REMOVE QUERY FIELDS
+    const queryObj = { ...req.query }; // create a shallow copy of query obj
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // BUILD QUERY
+    const query = await Tour.find(queryObj);
+    // const tours = await Tour.find({
+    //   duration: 5,
+    //   difficulty: 'easy',
+    // });
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
     res.status(200).json({
       status: 'success',
       results: tours.length,
