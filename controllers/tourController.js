25779e2d9a0417687fc4e9f8ee913de96ae1f8ce
Siddharth Shinde <siddharth.shinde@autodesk.com>
2023-03-04 15:00:15 -0500

    chore: implement get single tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -18,13 +18,21 @@ exports.getAllTours = async (req, res) => {
   }
 };
 
-exports.getTour = (req, res) => {
-  //const id = req.params.id * 1;
-  //const tour = tours.find((el) => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    //data: { tour: tour },
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
