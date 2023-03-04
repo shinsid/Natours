ba5716242449a66510d42e605bc4322adf4b3bed
Siddharth Shinde <siddharth.shinde@autodesk.com>
2023-03-04 14:26:15 -0500

    chore: clean up tourController for db implementation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,22 +1,4 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable node/no-unsupported-features/es-syntax */
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, _val) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid id',
-    });
-  }
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -32,34 +14,25 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: { tours: tours },
+    //results: tours.length,
+    //data: { tours: tours },
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+  //const id = req.params.id * 1;
+  //const tour = tours.find((el) => el.id === id);
   res.status(200).json({
     status: 'success',
-    data: { tour: tour },
+    //data: { tour: tour },
   });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = { id: newId, ...req.body };
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (_err) => {
-      res.status(201).json({
-        status: 'success',
-        data: { tour: newTour },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success',
+    //data: { tour: newTour },
+  });
 };
 
 exports.updateTour = (_req, res) => {
