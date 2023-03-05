a73fe5918c98e9e662449198b068eaf82d0a15cb
Siddharth Shinde <siddharth.shinde@autodesk.com>
2023-03-04 22:36:28 -0500

    chore: add pagination for tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -31,6 +31,17 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // const tours = await Tour.find({
     //   duration: 5,
     //   difficulty: 'easy',
