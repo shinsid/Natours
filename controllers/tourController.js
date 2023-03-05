2b2e198e3fddcf083e3b9ea77309c9826d98199a
Siddharth Shinde <siddharth.shinde@autodesk.com>
2023-03-04 22:22:25 -0500

    chore: add adv filtering, sorting, fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,13 +2,35 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    // REMOVE QUERY FIELDS
+    // Filtering
     const queryObj = { ...req.query }; // create a shallow copy of query obj
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
+    // Advance Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
     // BUILD QUERY
-    const query = await Tour.find(queryObj);
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // const tours = await Tour.find({
     //   duration: 5,
     //   difficulty: 'easy',
